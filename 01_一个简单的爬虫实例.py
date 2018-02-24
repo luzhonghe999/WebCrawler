cdd4c3dea1ffa1d6c1090152d35af2c35ccb8c34
luzhonghe <luzhonghe999@gmail.com>
2018-02-24 14:27:05 +0800

    提交

--- 01_一个简单的爬虫实例.py
+++ 01_一个简单的爬虫实例.py
@@ -1,5 +1,5 @@
-from urllib import request
+from urllib import request # 引用urllib中的request
 if __name__ == "__main__":
-    response = request.urlopen("http://58921.com")
-    html =str(response.read(), encoding='utf-8') 
+    response = request.urlopen("http://58921.com") #获取网址请求返回值
+    html =str(response.read(), encoding='utf-8') # 返回bytes转为utf8
     print(html)
